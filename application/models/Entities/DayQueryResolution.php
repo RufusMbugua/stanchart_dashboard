9783fca9aa587f4e0c7a179eeb08f8cd0db821b9
Rufus Ngugi Mbugua <mbuguarufus@gmail.com>
2013-07-19 03:11:45 +0300

    Getters and Setters

--- application/models/Entities/DayQueryResolution.php
+++ application/models/Entities/DayQueryResolution.php
@@ -119,5 +119,127 @@ class DayQueryResolution
      */
     private $may12;
 
+  
+    
+    public function getRecordid() {
+        return $this->recordid;
+    }
+
+    public function setRecordid($recordid) {
+        $this->recordid = $recordid;
+    }
+
+    public function getParameter() {
+        return $this->parameter;
+    }
+
+    public function setParameter($parameter) {
+        $this->parameter = $parameter;
+    }
+
+    public function getMay11() {
+        return $this->may11;
+    }
+
+    public function setMay11($may11) {
+        $this->may11 = $may11;
+    }
+
+    public function getJune11() {
+        return $this->june11;
+    }
+
+    public function setJune11($june11) {
+        $this->june11 = $june11;
+    }
+
+    public function getJuly11() {
+        return $this->july11;
+    }
+
+    public function setJuly11($july11) {
+        $this->july11 = $july11;
+    }
+
+    public function getAug11() {
+        return $this->aug11;
+    }
+
+    public function setAug11($aug11) {
+        $this->aug11 = $aug11;
+    }
+
+    public function getSept11() {
+        return $this->sept11;
+    }
+
+    public function setSept11($sept11) {
+        $this->sept11 = $sept11;
+    }
+
+    public function getOct11() {
+        return $this->oct11;
+    }
+
+    public function setOct11($oct11) {
+        $this->oct11 = $oct11;
+    }
+
+    public function getNov11() {
+        return $this->nov11;
+    }
+
+    public function setNov11($nov11) {
+        $this->nov11 = $nov11;
+    }
+
+    public function getDec11() {
+        return $this->dec11;
+    }
+
+    public function setDec11($dec11) {
+        $this->dec11 = $dec11;
+    }
+
+    public function getJan12() {
+        return $this->jan12;
+    }
+
+    public function setJan12($jan12) {
+        $this->jan12 = $jan12;
+    }
+
+    public function getFeb12() {
+        return $this->feb12;
+    }
+
+    public function setFeb12($feb12) {
+        $this->feb12 = $feb12;
+    }
+
+    public function getMar12() {
+        return $this->mar12;
+    }
+
+    public function setMar12($mar12) {
+        $this->mar12 = $mar12;
+    }
+
+    public function getApr12() {
+        return $this->apr12;
+    }
+
+    public function setApr12($apr12) {
+        $this->apr12 = $apr12;
+    }
+
+    public function getMay12() {
+        return $this->may12;
+    }
+
+    public function setMay12($may12) {
+        $this->may12 = $may12;
+    }
+
 
 }
